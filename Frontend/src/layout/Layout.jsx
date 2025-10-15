8a47b24d8872b591f50e45d4753d4f5a241e9891
JuanR04 <juancrg2004@hotmail.com>
2025-10-15 16:36:54 -0500

    feat(frontend): implementacion de layout

---
+++ Frontend/src/layout/Layout.jsx
@@ -0,0 +1,18 @@
+
+import "./layout.css";
+import Footer from "./footer/Footer";
+import Header from "./header/Header";
+import { Outlet } from 'react-router-dom';
+const Layout = () => {
+    return (
+        <div className="layout">
+            <Header />
+            <main className="layout-content">
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout
