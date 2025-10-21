0bd1a21fdd255e403bf3aaedaeedf91326fedb91
Lotorm <andresfelipe29portillo@gmail.com>
2025-10-21 01:36:24 -0500

    docs actualizacion de la base de datos
    se actualizó el .sql de la base de datos para que en user_password se almacene un codigo hash de hasta 256 caracteres, y se modificaron campos con nombres errones en productDetails

--- dbFiera.sql
+++ dbFiera.sql
@@ -37,11 +37,11 @@ id_product_details bigSerial PRIMARY KEY,
 id_product bigInt NOT NULL REFERENCES product(id_product) ON DELETE CASCADE,
 id_product_state bigInt NOT NULL REFERENCES product_state(id_product_state) ON DELETE CASCADE,
 id_color bigInt NOT NULL REFERENCES color(id_color) ON DELETE CASCADE,
-id_public_objetive varchar(50) NOT NULL,
-id_expertice varchar(50) NOT NULL ,
 product_price bigInt NOT NULL,
 stock bigInt NOT NULL,
-product_size varchar(50) NOT NULL
+product_size varchar(50) NOT NULL,
+public_objetive varchar(50) NOT NULL,
+expertice varchar(50) NOT NULL
 )
 -------------------tabla metodo de pago-----------------
 create table payment_method(
@@ -92,7 +92,7 @@ id_gender bigInt NOT NULL REFERENCES gender(id_gender),
 id_role bigInt NOT NULL REFERENCES role(id_role),
 full_name varchar(150) NOT NULL,
 email varchar(150) NOT NULL,
-user_password varchar(20) NOT NULL,
+user_password varchar(300) NOT NULL,
 phone_number varchar(20) NOT NULL,
 register_dare timeStamp WITHOUT TIME ZONE NOT NULL,
 modification_date timeStamp WITHOUT TIME ZONE NOT NULL,
