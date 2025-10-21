e547c06972b3080f8f54a4a2af3f7bed0e3d8fee
JuanR66803 <juan.reinosa@correounivalle.edu.co>
2025-10-21 12:43:32 -0500

    feat(fullstack): implementacion de logica para el resgistro de usuarios, con hash de contraseña

--- Backend/models/roleModel.js
+++ Backend/models/roleModel.js
@@ -2,29 +2,6 @@ import pool from "../config/db.js";
 
 export class RoleModel{
     
-    //Metodo que me permite crear una categoria
-    async createRole (role_name){
-    const query = `INSERT INTO role(role_name) VALUES($1) RETURNING *`;
-    const values= [role_name];
-    const result = await pool.query(query,values);
-    return result.rows[0];
-}
-
-    //Metodo que me permite actualizar una categoria
-    async updateRoleById (id_role,role_name){
-    const query = "UPDATE role SET role_name=$2 WHERE id_role=$1";
-    const values = [id_role, role_name];
-    const result = await pool.query(query,values);
-    return result.rows[0];
-}
-
-    //Metodo que me permite eliminar una categoria
-    async deleteRoleById (id_role){
-    const query = `DELETE FROM role where id_role= $1`;
-    const result = await pool.query(query,[id_role]);
-    return result.rows[0]
-
-}
 
 //Metodo que me permite obtener una categoria por su id
     async getRoleById (id_role){
@@ -35,9 +12,9 @@ export class RoleModel{
 }
 
 //Metodo que me permite obtener todas las categorias
-    async getAllRoles(){
-    const query = `SELECT * FROM role`;
-    const result = await pool.query(query);
+    async getRoleByName(role_name){
+    const query = `SELECT id_role FROM role WHERE role_name = $1`;
+    const result = await pool.query(query,[role_name]);
     return result.rows[0];
 }
 }
--- Backend/services/roleServices.js
+++ Backend/services/roleServices.js
@@ -3,31 +3,15 @@ import {RoleModel} from "../models/roleModel.js" ;
 const roleModel = new RoleModel();
 
 export class RoleService{
-    
-    //metodo para crear una categoria
-    async createRole(role_name){
-        // se crea la categoría sin comprobar si ya existe, hay que agregar un condicional
-        return (await roleModel.createRole(role_name));
-    };
-
-    //metodo para acrualizar una categoria
-    async updateRole(id_role,role_name){
-        return (await roleModel.updateRoleById(id_role, role_name));
-    };
-
-    //metodo para eliminar una categoria
-    async deleteRoleById(id_role){
-        return (await roleModel.deleteRoleById(id_role));
-    };
-
-    //metodo para obtener una categoria con su id
+   
+ 
     async getRoleById(id_role){
         return (await roleModel.getRoleById(id_role));
     };
 
-    //metodo para obtener todas las categorias
-    async getAllRoles(){
-        return (await roleModel.getAllRoles())
+
+    async getRoleByName(role_name){
+        return (await roleModel.getRoleByName(role_name))
     };
 }
 
